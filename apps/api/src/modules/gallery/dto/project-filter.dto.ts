adb4994e587e526ea67458d9b964017eaaee03a9
caxtonacollins <caxtonacollins@gmail.com>
2026-03-11 01:23:42 +0100

    feat(gallery): add project-filter dto

---
+++ apps/api/src/modules/gallery/dto/project-filter.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
+import { ProjectCategory } from '@app/database/entities/gallery-project.entity';
+
+export class ProjectFilterDto {
+  @ApiProperty({ enum: ProjectCategory, required: false })
+  @IsOptional()
+  @IsEnum(ProjectCategory)
+  category?: ProjectCategory;
+}
