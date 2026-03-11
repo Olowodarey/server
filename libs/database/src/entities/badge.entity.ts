2166db6286a61eb7ba380deaa794c9a0c994a1ff
caxtonacollins <caxtonacollins@gmail.com>
2026-03-11 01:19:02 +0100

    feat(database): add badge entity

---
+++ libs/database/src/entities/badge.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UserBadge } from './user-badge.entity';
+
+@Entity('badges')
+export class Badge {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ unique: true, length: 100 })
+  slug: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ name: 'image_url' })
+  imageUrl: string;
+
+  @Column({ name: 'xp_reward', default: 0 })
+  xpReward: number;
+
+  @Column({ name: 'trigger_condition', length: 255 })
+  triggerCondition: string;
+
+  @OneToMany(() => UserBadge, (ub) => ub.badge)
+  earnedBy: UserBadge[];
+}
