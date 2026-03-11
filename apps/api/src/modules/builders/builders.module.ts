b55c86f86fc5c7a7f1d0f26b11d4d20fe1e8412b
caxtonacollins <caxtonacollins@gmail.com>
2026-03-11 01:19:39 +0100

    feat(builders): add builders module

---
+++ apps/api/src/modules/builders/builders.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BuilderProfile } from '@app/database/entities/builder-profile.entity';
+import { BuildersController } from './builders.controller';
+import { BuildersService } from './builders.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([BuilderProfile])],
+  controllers: [BuildersController],
+  providers: [BuildersService],
+})
+export class BuildersModule {}
