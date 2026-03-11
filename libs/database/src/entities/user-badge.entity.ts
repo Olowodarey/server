c2e6f3a1959e64482374da47162ac5a493799b1e
caxtonacollins <caxtonacollins@gmail.com>
2026-03-11 01:19:02 +0100

    feat(database): add user-badge entity

---
+++ libs/database/src/entities/user-badge.entity.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, JoinColumn, PrimaryColumn } from 'typeorm';
+import { User } from './user.entity';
+import { Badge } from './badge.entity';
+
+@Entity('user_badges')
+export class UserBadge {
+  @PrimaryColumn({ name: 'user_id' })
+  userId: string;
+
+  @PrimaryColumn({ name: 'badge_id' })
+  badgeId: number;
+
+  @CreateDateColumn({ name: 'earned_at' })
+  earnedAt: Date;
+
+  @ManyToOne(() => User, (u) => u.badges)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Badge, (b) => b.earnedBy)
+  @JoinColumn({ name: 'badge_id' })
+  badge: Badge;
+}
