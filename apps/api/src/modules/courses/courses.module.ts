1b1f2c107d68ac4dda373fafa3470f6852a93980
caxtonacollins <caxtonacollins@gmail.com>
2026-03-11 01:19:45 +0100

    feat(courses): add courses module

---
+++ apps/api/src/modules/courses/courses.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserProgress } from '@app/database/entities/user-progress.entity';
+import { CoursesController } from './courses.controller';
+import { CoursesService } from './courses.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserProgress])],
+  controllers: [CoursesController],
+  providers: [CoursesService],
+  exports: [CoursesService],
+})
+export class CoursesModule {}
